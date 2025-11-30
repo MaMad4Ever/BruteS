d77c1adb4b74d6a566132f131bf6662d8cbf0687
MaMad <113606119+MaMad4Ever@users.noreply.github.com>
2025-11-30 06:46:16 +0330

    Enhance domain scanner with silent mode and output and time out
    Added silent mode and output file option for reachable URLs.

--- Brutes.py
+++ Brutes.py
@@ -3,9 +3,9 @@ import argparse
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import os
 
-def check_subdomain(url):
+def check_subdomain(url, timeout):
     try:
-        response = requests.get(url, timeout=5)
+        response = requests.get(url, timeout=timeout)
         if response.status_code == 200:
             return url, 'reachable'
         else:
@@ -17,9 +17,10 @@ def check_subdomain(url):
     except Exception as e:
         return url, f'error: {e}'
 
-def domain_scanner(domain_name, sub_domain_names, show_unreachable, max_workers, mode):
-    print('----------- Scanner Started -----------')
-    print('---- URL after scanning subdomains ----')
+def domain_scanner(domain_name, sub_domain_names, show_unreachable, max_workers, mode, silent, output_file, timeout):
+    if not silent:
+        print('----------- Scanner Started -----------')
+        print('---- URL after scanning subdomains ----')
 
     reachable_urls = []
     unreachable_urls = []
@@ -27,28 +28,39 @@ def domain_scanner(domain_name, sub_domain_names, show_unreachable, max_workers,
     urls = [f"{mode}://{subdomain}.{domain_name}" for subdomain in sub_domain_names]
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        future_to_url = {executor.submit(check_subdomain, url): url for url in urls}
+        future_to_url = {executor.submit(check_subdomain, url, timeout): url for url in urls}
         
         for future in as_completed(future_to_url):
             url, status = future.result()
             if status == 'reachable':
                 reachable_urls.append(url)
-                print(f'[+] {url} - Reachable')
+                clean = url.replace(f"{mode}://", "")
+
+                if output_file:
+                    with open(output_file, "a") as f:
+                        f.write(clean + "\n")
+
+                if silent:
+                    print(clean)
+                else:
+                    print(f'[+] {url} - Reachable')
             elif show_unreachable:
                 unreachable_urls.append(url)
-                print(f'[-] {url} - {status}')
+                if not (silent):
+                    print(f'[-] {url} - {status}')
 
-    print('\nReachable URLs:')
-    for url in reachable_urls:
-        print(url)
-
-    if show_unreachable:
-        print('\nUnreachable URLs:')
-        for url in unreachable_urls:
+    if not (silent):
+        print('\nReachable URLs:')
+        for url in reachable_urls:
             print(url)
 
-    print('\n---- Scanning Finished ----')
-    print('----- Scanner Stopped -----')
+        if (show_unreachable):
+            print('\nUnreachable URLs:')
+            for url in unreachable_urls:
+                print(url)
+
+        print('\n---- Scanning Finished ----')
+        print('----- Scanner Stopped -----')
 
 
 if __name__ == '__main__':
@@ -58,6 +70,12 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--threads', type=int, default=10, help='Number of threads to use (default: 10)')
     parser.add_argument('-w', '--wordlist', type=str, default='subdomain_names.txt', help='Path to the wordlist file (default: subdomain_names.txt)')
     parser.add_argument('-m', '--mode', choices=['http', 'https'], default='https', help='Protocol to use (default: https)')
+    parser.add_argument('-s', '--silent', action='store_true', help='Run scanner silently')
+    parser.add_argument('-o', '--output', type=str, help='Save reachable subdomains to a file')
+    parser.add_argument('--timeout', type=int, default=5, help='Request timeout in seconds (default: 5)')
+
+
+
     args = parser.parse_args()
 
     dom_name = args.domain.strip()
@@ -65,7 +83,11 @@ if __name__ == '__main__':
     max_workers = args.threads
     wordlist_path = args.wordlist.strip()
     mode = args.mode.strip()
-    print('\n')
+    silent = args.silent
+    output_file = args.output
+    timeout = args.timeout
+    if not (silent):
+        print('\n')
 
     if not os.path.isfile(wordlist_path):
         print(f"Error: The wordlist file '{wordlist_path}' does not exist.")
@@ -74,6 +96,6 @@ if __name__ == '__main__':
     try:
         with open(wordlist_path, 'r') as file:
             sub_dom = [line.strip() for line in file if line.strip()]
-        domain_scanner(dom_name, sub_dom, show_unreachable, max_workers, mode)
+        domain_scanner(dom_name, sub_dom, show_unreachable, max_workers, mode, silent, output_file, timeout)
     except FileNotFoundError:
         print("Error: 'subdomain_names.txt' file not found.")
